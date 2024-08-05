ba01efae01f144fd6243d278a02a80798214f1c2
Sivakumar Murugesan <43994260+shivuat@users.noreply.github.com>
2024-08-05 14:41:10 -0400

    Update dg-v9.js

--- dg-v9.js
+++ dg-v9.js
@@ -11,7 +11,7 @@
   }
   // Create and style the mic button
   var micButton = document.createElement('button');
-  micButton.id = 'micButton';
+  micButton.id = '🎤';
   micButton.innerHTML = ':microphone:';
   micButton.style = 'position: fixed; bottom: 20px; right: 20px; z-index: 9999; background-color: white; color: black; border: none; border-radius: 50%; width: 60px; height: 60px; font-size: 24px; cursor: pointer;';
   micButton.draggable = true;
@@ -256,5 +256,5 @@
     return false;
   });
   // Display stored intent on page load
-  displayStoredIntent();
+  //displayStoredIntent();
 })();
